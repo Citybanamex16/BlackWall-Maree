69fcd163a36cd528df275efea5c631b400c23dee
JPJuarz <jpjuarez@gmail.com>
2026-03-04 12:59:39 -0600

    Admin Dashboard v1.1

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Dashboard principal
 router.get('/', (req, res) => {
-    res.render('admin/admindashboard');
-});
+  res.render('admin/admindashboard')
+})
 
 // Cada sección del dashboard uu que rico
 router.get('/ingredients', (req, res) => {
-    res.render('admin/ingredients');
-});
+  res.render('admin/ingredients')
+})
 
 router.get('/products', (req, res) => {
-    res.render('admin/products');
-});
+  res.render('admin/products')
+})
 
 router.get('/royalty', (req, res) => {
-    res.render('admin/royalty');
-});
+  res.render('admin/royalty')
+})
 
 router.get('/collaborators', (req, res) => {
-    res.render('admin/collaborators');
-});
+  res.render('admin/collaborators')
+})
 
 router.get('/orders', (req, res) => {
-    res.render('admin/orders');
-});
+  res.render('admin/orders')
+})
 
 router.get('/promotions', (req, res) => {
-    res.render('admin/promotions');
-});
+  res.render('admin/promotions')
+})
 
 router.get('/events', (req, res) => {
-    res.render('admin/events');
-});
+  res.render('admin/events')
+})
 
-module.exports = router;
+module.exports = router
