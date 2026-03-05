d58da3aef9ac0547514d1eab9b1ff022ca7aca88
JPJuarz <jpjuarez@gmail.com>
2026-03-04 21:17:41 -0600

    Menu v1.0

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -5,6 +5,10 @@ const router = express.Router()
 // Llamadas de controlador
 const clienteControlador = require('../controllers/cliente.controller.js')
 
+router.get('/', (req, res) => {
+  res.render('menu')
+})
+
 // Middlewares de enrutamiento -> router.metodo('/url', controlador.metodo)
 
 router.get('/', clienteControlador.get_main)
