bf34722775271708e648fe61c4861884e6c04161
JPJuarz <jpjuarez@gmail.com>
2026-03-05 19:15:50 -0600

    Basic HTML for each page (mapa del sitio)

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -1,16 +1,11 @@
-// Configuración Express y enrutamiento
 const express = require('express')
 const router = express.Router()
+// const clienteControlador = require('../controllers/cliente.controller.js')
 
-// Llamadas de controlador
-const clienteControlador = require('../controllers/cliente.controller.js')
-
-router.get('/', (req, res) => {
-  res.render('menu')
-})
-
-// Middlewares de enrutamiento -> router.metodo('/url', controlador.metodo)
-
-router.get('/', clienteControlador.get_main)
+router.get('/hours', (req, res) => res.render('cliente/hours'))
+router.get('/promotions', (req, res) => res.render('cliente/promotions'))
+router.get('/record', (req, res) => res.render('cliente/record'))
+router.get('/royalty', (req, res) => res.render('cliente/royalty'))
+router.get('/order', (req, res) => res.render('cliente/order'))
 
 module.exports = router
