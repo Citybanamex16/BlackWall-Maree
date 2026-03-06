cdf4c72cd52d1a5a79e66383c101ce3e549fce8b
JPJuarz <jpjuarez@gmail.com>
2026-03-05 19:15:50 -0600

    Basic HTML for each page (mapa del sitio)

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,38 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-// Dashboard principal
-router.get('/', (req, res) => {
-  res.render('admin/admindashboard')
-})
-
-// Cada sección del dashboard uu que rico
-router.get('/ingredients', (req, res) => {
-  res.render('admin/ingredients')
-})
-
-router.get('/products', (req, res) => {
-  res.render('admin/products')
-})
-
-router.get('/royalty', (req, res) => {
-  res.render('admin/royalty')
-})
-
-router.get('/collaborators', (req, res) => {
-  res.render('admin/collaborators')
-})
-
-router.get('/orders', (req, res) => {
-  res.render('admin/orders')
-})
-
-router.get('/promotions', (req, res) => {
-  res.render('admin/promotions')
-})
-
-router.get('/events', (req, res) => {
-  res.render('admin/events')
-})
+router.get('/', (req, res) => res.render('admin/admindashboard'))
+router.get('/ingredients', (req, res) => res.render('admin/ingredients'))
+router.get('/products', (req, res) => res.render('admin/products'))
+router.get('/royalty', (req, res) => res.render('admin/royalty'))
+router.get('/collaborators', (req, res) => res.render('admin/collaborators'))
+router.get('/orders', (req, res) => res.render('admin/orders'))
+router.get('/promotions', (req, res) => res.render('admin/promotions'))
+router.get('/events', (req, res) => res.render('admin/events'))
+router.get('/whatsapp', (req, res) => res.render('admin/whatsapp'))
+router.get('/cliente', (req, res) => res.render('menu'))
 
 module.exports = router
