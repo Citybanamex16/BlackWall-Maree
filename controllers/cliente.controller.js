34116decc6d8b8f0f696e3069c9a617b361c500b
Citybanamex16 <carlosdc2005@icloud.com>
2026-03-25 06:41:21 -0600

    Fin de Sesiones

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -1,5 +1,4 @@
 // Modulos nativos
-const querystring = require('node:querystring')
 // const path = require('path')
 
 // Llamar al model
@@ -10,26 +9,24 @@ const nav = require('../models/breadcrumbs.model.js')
 
 exports.getMenu = (request, response, next) => {
   const breadcrumbs = nav.getBreadcrumbs('Menu')
-  response.render('cliente/menu', { breadcrumbs})
+  response.render('cliente/menu', { breadcrumbs })
 }
 
-
-
 exports.logout = (request, response, next) => {
-    // 1. Destruye la sesión en el servidor
-    console.log("Llamando a logout")
-    request.session.destroy((err) => {
-        if (err) {
-            console.error("Error al cerrar sesión:", err);
-            return next(err);
-        }
-        // 2. Limpia la cookie del navegador (el nombre por defecto es 'connect.sid')
-        response.clearCookie('connect.sid');
-        
-        // 3. Redirige al login o al inicio
-        response.redirect('/cliente/menu'); 
-    });
-};
+  // 1. Destruye la sesión en el servidor
+  console.log('Llamando a logout')
+  request.session.destroy((err) => {
+    if (err) {
+      console.error('Error al cerrar sesión:', err)
+      return next(err)
+    }
+    // 2. Limpia la cookie del navegador (el nombre por defecto es 'connect.sid')
+    response.clearCookie('connect.sid')
+
+    // 3. Redirige al login o al inicio
+    response.redirect('/cliente/menu')
+  })
+}
 
 exports.getRoyalty = (request, response, next) => {
   const breadcrumbs = nav.getBreadcrumbs('Royalty')
@@ -53,12 +50,8 @@ exports.postLogin = (request, response, next) => {
 
   // 3. respondemos
   response.redirect('/cliente/menu')
-
 }
 
 exports.logOut = (request, response, next) => {
 
 }
-
-
-
