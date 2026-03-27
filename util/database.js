999e2d4471d4eafcd5714cb73cb8ac0b68bf129e
Santiago Acosta <santiago.acoes.4@gmail.com>
2026-03-26 19:10:25 -0600

    cancela pedido task terminada

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2')
 
 // Pool de conexiones
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'Maree_prueba2',
-  password: '',
+  host: process.env.HOST || 'localhost',
+  user: process.env.USER || 'root',
+  database: process.env.DB || 'MareeDB',
+  password: process.env.PASSWORD || '',
   waitForConnection: true,
   connectionLimit: 10
 
