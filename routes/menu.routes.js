ec3f4f00f6531c4723178e45599486c58330e13a
JPJuarz <jpjuarez@gmail.com>
2026-03-26 21:08:29 -0600

    Registrar Orden V2

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = express.Router()
-// const clienteControlador = require('../controllers/menu.controller.js')
-
-// Llamadas de controlador
 const menuControlador = require('../controllers/menu.controlador.js')
 
 router.get('/menu', menuControlador.getMenu)
 router.get('/orden', menuControlador.getOrden)
-
 router.get('/consultaplatillo', menuControlador.getPlatillo)
 router.post('/agregaritem', menuControlador.agregarItem)
+router.post('/pedidos/validar', menuControlador.validarPedido)
+router.post('/pedidos/confirmar', menuControlador.confirmarPedido)
 
 module.exports = router
