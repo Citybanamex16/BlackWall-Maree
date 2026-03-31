b88ebcd054fa0144a20d49b8e68f64f2000b7d2d
M4nsi00 <sebastian.mansillacots@gmail.com>
2026-03-30 20:26:16 -0600

    Base de Datos FINAL funcionando y con datos de ejemplo

---
+++ util/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2')
+
+// Pool de conexiones
+const pool = mysql.createPool({
+  // Si usas windows o mac usa localhost, el de abajo lo usas en caso de tener una WSL y cambia
+  // para cada persona
+  // host: 'localhost',
+  host: '172.20.96.1',
+  user: 'root',
+  database: 'MareeBD',
+  password: '',
+  waitForConnection: true,
+  connectionLimit: 10
+
+})
+
+module.exports = pool.promise() // Habilitamos poder usar .then() & .catch() yuhhhh
