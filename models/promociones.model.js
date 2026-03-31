fab8800fd3e0c69394472b868b98e8356237f035
amazingly145 <A01753419@tec.mx>
2026-03-30 21:48:11 -0600

    Agregar Errores si no se escribió un string

--- models/promociones.model.js
+++ models/promociones.model.js
@@ -1,14 +1,20 @@
 const db = require('../util/database')
 module.exports = class Promociones {
-  constructor (miNombre, miDescuento, miCondicion) {
+  constructor (miId, miNombre, miDescuento, miCondicion, miActivo, miFechaInicio, miFechaFinal) {
+    this.id = miId
     this.nombre = miNombre
     this.descuento = miDescuento
     this.condicion = miCondicion
+    this.Activo = miActivo
+    this.fechaInicio = miFechaInicio
+    this.fechaFinal = miFechaFinal
   }
 
   save () {
-    return db.execute('INSERT INTO promociones (nombre, descuento, condiciones) VALUES (?,?,?)',
-      [this.nombre, this.descuento, this.condicion]
+    console.log('Se ha logrado subir a la base de datos')
+    return db.execute('INSERT INTO promociones (ID_promocion, Nombre, Descuento, ' +
+        'Condiciones, Activo, FechaInicio, FechaFinal), VALUES (?,?,?,?,?,?,?)',
+    [this.nombre, this.descuento, this.condicion]
     )
   }
 
