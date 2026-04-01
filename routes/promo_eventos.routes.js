e09d9da55c4b20c606509228a32e0cc83e319568
Andrea_Cantu <A01753419@tec.mx>
2026-04-01 12:37:44 -0600

    Revert "20 registrar promo"

--- routes/promo_eventos.routes.js
+++ routes/promo_eventos.routes.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const evenPromoControlador = require('../controllers/promo_eventos.controller.js')
+const evenPromoControlador = require('../controllers/promo_eventos.controlador.js')
 
 router.get('/eventos', evenPromoControlador.getEvents)
 
 router.get('/eventos/api/all', evenPromoControlador.getEventsAPI)
 
 router.post('/eventos/registrar', evenPromoControlador.postRegistrarEvento)
-
-router.post('/promociones', evenPromoControlador.postRegistrarPromotions)
-
-router.get('/promociones/producto-filtro', evenPromoControlador.getFilterProductos)
 
 router.get('/eventos/api/catalogos', evenPromoControlador.getCatalogosEvento)
 
