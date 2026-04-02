9645b9d96eeafdb6567ad8766a25cfeff1cd3f82
Luis Fernando <a01708712@tec.mx>
2026-04-01 18:16:08 -0600

    Funcionalidad de dar de baja a colaborador arreglada posterior a cambios en la BD y en el código

--- util/database.js
+++ util/database.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
   // Si usas windows o mac usa localhost, el de abajo lo usas en caso de tener una WSL y cambia
   // para cada persona
-  host: 'mysql-maree2.alwaysdata.net',
-  user: 'maree2',
-  database: 'maree2_1',
-  password: 'maree123',
+  host: 'localhost',
+  user: 'root',
+  database: 'maree',
+  password: '',
   waitForConnection: true,
   connectionLimit: 10
 
