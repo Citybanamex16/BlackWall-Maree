83ba68206705b3b205093758ebb56f3af3014fe1
amazingly145 <A01753419@tec.mx>
2026-04-03 09:14:43 -0600

    Se agregócontrolador, modelo y vista para visualizar sistema royalty

--- models/royalty.model.js
+++ models/royalty.model.js
@@ -2,16 +2,16 @@ const db = require('../util/database.js')
 
 module.exports = class Royalty {
   constructor (nombre, numeroPrioridad, descripcion, maxVisitas, minVisitas) {
-    this.Nombre_Royaltyombre_Royalty = nombre
+    this.Nombre_Royalty = nombre
     this.Número_de_prioridad = numeroPrioridad
-    this.Descripciónescripción = descripcion
+    this.Descripción = descripcion
     this.Max_Visitas = maxVisitas
     this.Min_Visitas = minVisitas
   }
 
   // Admin
   static fetchAll () {
-    return db.execute('SELECT * FROM estado_royalty')
+    return db.execute('SELECT * FROM estado_royalty ORDER BY Número_de_prioridad ASC')
   }
 
   // Cliente
