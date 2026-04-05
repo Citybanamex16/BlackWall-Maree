dcd241c8fa7f45f3461db6f790b2fa9b18e3f9f9
amazingly145 <A01753419@tec.mx>
2026-04-04 19:44:01 -0600

    Se cambio para que se renderizaran archivos JSON

--- public/js/Royalty/royalty.admin.js
+++ public/js/Royalty/royalty.admin.js
@@ -11,3 +11,43 @@ function borrarRoyalty (NombreRoyalty) {
     })
   }
 }
+
+async function cargarRoyalty () {
+  try {
+    const response = await fetch('/royalty/royaltyAdmin/api')
+    const data = await response.json()
+
+    console.log('Mostrando los royalties en HTML')
+    const container = document.getElementById('royaltyContainer')
+    data.data.forEach(royalty => {
+      container.innerHTML += `
+        <div class="column is-half">
+          <div class="card">
+            <div class="card-content">
+              <p class="title is-5">${royalty.Nombre_Royalty}</p>
+              <div class="content">
+                <p><strong>Prioridad:</strong> ${royalty.Número_de_prioridad}</p>
+                <p><strong>Descripción:</strong> ${royalty.Descripción}</p>
+                <p><strong>Visitas mínimas:</strong> ${royalty.Min_Visitas}</p>
+                <p><strong>Visitas máximas:</strong> ${royalty.Max_Visitas}</p>
+              </div>
+              <footer class="card-footer">
+                <button class="button is-warning card-footer-item" onclick="modificarRoyalty('${royalty.Nombre_Royalty}')">
+                  <span class="icon"><i class="fas fa-pen"></i></span>
+                  <span>Modificar</span>
+                </button>
+                <button class="button is-danger card-footer-item" onclick="borrarRoyalty('${royalty.Nombre_Royalty}')">
+                  <span class="icon"><i class="fas fa-trash"></i></span>
+                  <span>Borrar</span>
+                </button>
+              </footer>
+            </div>
+          </div>
+        </div>
+      `
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+cargarRoyalty()
