ae924508e19a12230bacc22cc740725aea7ecc93
M4nsi00 <sebastian.mansillacots@gmail.com>
2026-04-06 10:24:58 -0600

    Revert "Merge branch 'visualizarRoyaltyCL' into dev"
    This reverts commit 1a6cd7983d54ed2219c58c20dba627e9f135b7e9, reversing
    changes made to 3d77d9c8236237cd11788666df306d5733e4fcf1.

--- public/js/admin.js
+++ public/js/admin.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const id = boton.dataset.id
       const nombre = boton.dataset.nombre
 
-      const confirmado = window.confirm(`¿Seguro que deseas dar de baja a ${nombre}?`)
+      const confirmado = confirm(`¿Seguro que deseas dar de baja a ${nombre}?`)
 
       if (!confirmado) {
         return
