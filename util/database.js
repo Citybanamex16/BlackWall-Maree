473769ce5fd46c258e7b7d3c4a10d82c48ebeac1
Citybanamex16 <carlosdc2005@icloud.com>
2026-04-07 15:48:40 -0600

    Setup de modulo

--- util/database.js
+++ util/database.js
@@ -1,11 +1,33 @@
 const mysql = require('mysql2')
 
-// Pool de conexiones
+/* Pool de conexiones
 const pool = mysql.createPool({
   host: 'localhost',
   // host: '172.20.96.1', // NO LO BORREN SOLO COMENTENLO
   // port: '3307',
-  port: '3306',
+  port: '3000',
+  user: 'root',
+  database: 'mareebd',
+  password: '',
+  waitForConnection: true,
+  connectionLimit: 10
+
+})
+*/
+
+/* ==Contraseñas y usuario ==
+Usuario: CL85565990
+Pass: CL006901!
+
+*/
+
+
+
+//Charly 
+
+// Pool de conexiones
+const pool = mysql.createPool({
+  host: 'localhost',
   user: 'root',
   database: 'mareebd',
   password: '',
