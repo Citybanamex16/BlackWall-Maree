9e46f4ff35d0f0956b3912f0bc4fcf89ea7aece6
JPJuarz <jpjuarez@gmail.com>
2026-04-17 23:47:58 -0600

    Visualizar Feedback v2

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const menuControlador = require('../controllers/menu.controlador.js')
-const feedBackControlador = require('../controllers/feedback.controlador.js')
 
 // Rutas Menu cliente
 router.get('/menu', menuControlador.getMenu)
@@ -22,10 +21,6 @@ router.put('/modifProduct/:id', menuControlador.postModifProduct) // ¡ Ese es P
 // Eliminar Producto
 router.delete('/eliminarProducto', menuControlador.deleteProducto)
 router.put('/desactivarProducto', menuControlador.putDesactivarProducto)
-
-// rutas feedback
-router.get('/feedback', feedBackControlador.getFeedback)
-router.get('/feedbackCatalog', feedBackControlador.getFeedbackCatalog)
 
 // Rutas Global: Rutas utilizadas por cualquier CU, devuelven información general
 router.get('/globalAdminIngredientes', menuControlador.getIngredientesFullCatalog)
