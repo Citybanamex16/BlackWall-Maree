c17dcbc5da305b8f8e39a782565a7edbd15f76d8
amazingly145 <A01753419@tec.mx>
2026-04-21 12:59:20 -0600

    Agregar Google Wallet

--- util/database.js
+++ util/database.js
@@ -2,14 +2,11 @@ const mysql = require('mysql2')
 
 // Pool de conexiones
 const pool = mysql.createPool({
-  // host: 'localhost',
-  host: '172.20.96.1', // NO LO BORREN SOLO COMENTENLO
-  port: '3300',
-  // port: '3000',
+  host: 'localhost',
   user: 'root',
   database: 'mareebd',
-  password: '1234',
-  waitForConnection: true,
+  password: '',
+  waitForConnections: true,
   connectionLimit: 10
 
 })
